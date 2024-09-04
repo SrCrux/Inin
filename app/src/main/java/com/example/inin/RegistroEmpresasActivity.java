0971b0ac01a61ba6e1bf76a2c860adeefbbf82e9
SrCrux <pablo.guipe@gmail.com>
2024-09-04 15:17:36 +0200

    TextInputLayaout definida

--- app/src/main/java/com/example/inin/RegistroEmpresasActivity.java
+++ app/src/main/java/com/example/inin/RegistroEmpresasActivity.java
@@ -1,27 +1,32 @@
 package com.example.inin;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
 
 public class RegistroEmpresasActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registro_empresas);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+        EditText editText = findViewById(R.id.nombreEmpresaEditText);
 
+        // Obtén el drawable del cursor
+        Drawable cursorDrawable = ContextCompat.getDrawable(this, R.drawable.custom_cursor);
 
+        // Establece el drawable del cursor
+        if (cursorDrawable != null) {
+            editText.setTextCursorDrawable(cursorDrawable);
+        }
     }
 }
